6589e6a7c07c7a88df5148dbf654c5e1ec525a9c
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-02-03 13:02:57 +0000

    database done..for now 4/50

--- db/score_tracker.sql
+++ db/score_tracker.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS teams;
 CREATE TABLE teams (
     id SERIAL PRIMARY KEY,
     team_name VARCHAR(255),
-    games_id INT NOT NULL REFERENCES games(id) ON DELETE CASCADE
+    points INT,
+    wins INT,
+    losses INT
 );
 
 CREATE TABLE games (
